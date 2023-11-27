cc45f5cf3f2320a86b05cad24e2590b5d4abd2eb
Dmitry Ivanizki <dmitry.ivanizki@gmail.com>
2023-11-27 09:12:56 +0100

    Sorted constants by their position in the ASCII table.

--- src/main/java/ivanizki/research/data/ASCII.java
+++ src/main/java/ivanizki/research/data/ASCII.java
@@ -6,44 +6,108 @@ package ivanizki.research.data;
 @SuppressWarnings("javadoc")
 public interface ASCII {
 
-	public static final String AMPERSAND = "&";
-
-	public static final String BACKSLASH = "\\";
-
-	public static final String COLON = ":";
-
-	public static final String EQUAL = "=";
-
-	public static final String GREATER = ">";
-
-	public static final String LESS = "<";
-
-	public static final String MINUS = "-";
-
+	// 13 CR
 	public static final String NEWLINE = "\n";
 
-	public static final String PAR_BEGIN = "{";
-
-	public static final String PAR_END = "}";
-
-	public static final String QUOTE = "\"";
-
-	public static final String ROUND_BRACKET_BEGIN = "(";
-
-	public static final String ROUND_BRACKET_END = ")";
-
-	public static final String SHARP = "#";
-
-	public static final String SLASH = "/";
-
+	// 32
 	public static final String SPACE = " ";
 
-	public static final String SQRT = "√";
+	// 33
+	public static final String EXCLAMATION = "!";
 
+	// 34
+	public static final String QUOTE = "\"";
+
+	// 35
+	public static final String SHARP = "#";
+
+	// 36
+	public static final String DOLLAR = "$";
+
+	// 37
+	public static final String PERCENT = "%";
+
+	// 38
+	public static final String AMPERSAND = "&";
+
+	// 39
+	public static final String TICK = "'";
+
+	// 40
+	public static final String ROUND_BRACKET_BEGIN = "(";
+
+	// 41
+	public static final String ROUND_BRACKET_END = ")";
+
+	// 42
+	public static final String STAR = "*";
+
+	// 43
+	public static final String PLUS = "+";
+
+	// 44
+	public static final String COMMA = ",";
+
+	// 45
+	public static final String MINUS = "-";
+
+	// 46
+	public static final String DOT = ".";
+
+	// 47
+	public static final String SLASH = "/";
+
+	// 58
+	public static final String COLON = ":";
+
+	// 59
+	public static final String SEMICOLON = ";";
+
+	// 60
+	public static final String LESS = "<";
+
+	// 61
+	public static final String EQUAL = "=";
+
+	// 62
+	public static final String GREATER = ">";
+
+	// 63
+	public static final String QUESTION = "?";
+
+	// 64
+	public static final String AT = "@";
+
+	// 91
 	public static final String SQUARE_BRACKET_BEGIN = "[";
 
+	// 92
+	public static final String BACKSLASH = "\\";
+
+	// 93
 	public static final String SQUARE_BRACKET_END = "]";
 
+	// 94
+	public static final String HAT = "^";
+
+	// 95
 	public static final String UNDERSCORE = "_";
+
+	// 96
+	public static final String BACKTICK = "`";
+
+	// 123
+	public static final String PAR_BEGIN = "{";
+
+	// 124
+	public static final String SEPARATOR = "|";
+
+	// 125
+	public static final String PAR_END = "}";
+
+	// 126
+	public static final String TILDE = "~";
+
+	public static final String SQRT = "√";
 
 }
