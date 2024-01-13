1f273863074ca59387c2b4530234fd38a037661a
Dmitry Ivanizki <dmitry.ivanizki@gmail.com>
2024-01-13 11:08:45 +0100

    Improved resources for the file path button column.

---
+++ src/main/java/ivanizki/research/layout/manuscript/I18NConstants.java
@@ -0,0 +1,19 @@
+package ivanizki.research.layout.manuscript;
+
+import com.top_logic.basic.util.ResKey;
+import com.top_logic.layout.I18NConstantsBase;
+
+/**
+ * Internationalization constants for this package.
+ *
+ * @author ivanizki
+ */
+@SuppressWarnings("javadoc")
+public class I18NConstants extends I18NConstantsBase {
+
+	public static ResKey MANUSCRIPT_FILE_PATH_CLIPBOARD_HANDLER_LABEL;
+
+	static {
+		initConstants(I18NConstants.class);
+	}
+}
