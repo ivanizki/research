ad092e026a21061ab540b0483f4f42e5d45503ed
Dmitry Ivanizki <dmitry.ivanizki@gmail.com>
2024-02-23 10:29:28 +0100

    Added preliminary view for authors.

---
+++ src/main/java/ivanizki/research/layout/manuscript/AuthorTableConfigurationProvider.java
@@ -0,0 +1,27 @@
+package ivanizki.research.layout.manuscript;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.top_logic.layout.table.model.NoDefaultColumnAdaption;
+import com.top_logic.layout.table.model.TableConfiguration;
+
+import ivanizki.research.model.Model;
+import ivanizki.research.model.ModelType;
+
+/**
+ * {@link NoDefaultColumnAdaption} for the table of {@link ModelType#AUTHOR}s.
+ *
+ * @author ivanizki
+ */
+public class AuthorTableConfigurationProvider extends NoDefaultColumnAdaption {
+
+	@Override
+	public void adaptConfigurationTo(TableConfiguration table) {
+		List<String> defaultColumns = new ArrayList<>();
+		defaultColumns.add(Model.NAME);
+		defaultColumns.add(Model.TOPICS);
+		table.setDefaultColumns(defaultColumns);
+	}
+
+}
